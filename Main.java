7fc197c2d892786dea1e5e91e513f181e2e69857
tomad6909 <tomad6909@wrdsb.ca>
2020-09-14 14:02:23 +0000

    Finished question A1Q1

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program converts degrees celcius to degrees farenheit
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +10,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    // ask user for the degrees celcius
+    System.out.println("Please enter the degrees celcius:");
+
+    // declare and initialize a variable for the degrees celcius
+    int degreesCelcius = input.nextInt();
+
+    int degreesFarenheit = (degreesCelcius * 9) / 5 + 32;
+
+    // tell the user the degrees farenheit
+    System.out.println("It is " + degreesFarenheit + " outside.");
+
+
   }
 }
